bb53181c7eaaa93e9bd74e4eab5f7f9ba8192aae
unknown <wandersonst7dev@gmail.com>
2024-05-14 08:46:57 -0300

    Strings

---
+++ string-type/string-type.js
@@ -0,0 +1,10 @@
+"use strict";
+// ===> Exemplo 01 - Single Quotes
+let nomeCompleto = 'Wanderson Santos';
+console.log(nomeCompleto);
+// ===> Exemplo 02 - Double Quotes
+let funcaoEmpresa = "Programador";
+console.log(funcaoEmpresa);
+// ===> Exemplo 03 - Back Ticks
+let dadosFuncionario = `${nomeCompleto} trabalha como ${funcaoEmpresa}.`;
+console.log('Dados do Funcionário: ' + dadosFuncionario);
---
+++ string-type/string-type.ts
@@ -0,0 +1,11 @@
+// ===> Exemplo 01 - Single Quotes
+let nomeCompleto: string = 'Wanderson Santos';
+console.log(nomeCompleto);
+
+// ===> Exemplo 02 - Double Quotes
+let funcaoEmpresa: string = "Programador";
+console.log(funcaoEmpresa)
+
+// ===> Exemplo 03 - Back Ticks
+let dadosFuncionario:string = `${nomeCompleto} trabalha como ${funcaoEmpresa}.`;
+console.log('Dados do Funcionário: ' + dadosFuncionario)
